b266b1063f0a3e03e1c7d71a8903b16117271491
giulianotech <giulimp2@gmail.com>
2025-08-06 15:44:45 -0300

    O código da implementação do deposito está completa e funcional

--- desafio.py
+++ desafio.py
@@ -13,22 +13,35 @@ extrato = ""
 numero_saques = 3
 
 while True:
-    opcao = input(menu)
+    opcao = input(menu + "Escolha uma opção: ").lower()
+    
 
     if opcao == "d":
        
             print("Depósito.")
-
+            deposito = float(input("Digite o valor do depósito: "))
+            
+            saldo += deposito
+            print(f"Depósito de R$ {deposito:.2f} realizado com sucesso.")
+            
+            
     elif opcao == "s":
-       
+            
             print("saque.")
+            saque = float(input("Digite o valor do saque: "))  
+            if saque != numero_saques:
+                  
+                  print("Número de saques excedido.")
+            elif saque > limite:
+                  print("Valor do saque excede o limite.")
 
+                         
+    
     elif opcao == "e":
        
-        print("Extrato.")
+         print("Extrato." + f"\nSaldo: R$ {saldo:.2f}") 
 
     elif opcao == "q":
       
         break
 
-    
